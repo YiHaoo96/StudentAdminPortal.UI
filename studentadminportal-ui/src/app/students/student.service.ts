ca9d3da94aa431ae80c46d1c874a0fe84df609e4
YiHaoo96 <leeyihao96@gmail.com>
2023-03-19 14:44:44 +0800

    add enviroment file and move apiUrl to environment

--- studentadminportal-ui/src/app/students/student.service.ts
+++ studentadminportal-ui/src/app/students/student.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { AddStudentRequest } from '../models/api-models/add-student.model';
 import { Student } from '../models/api-models/student.model';
 import { UpdateStudentRequest } from '../models/api-models/update-student.model';
@@ -9,7 +10,7 @@ import { UpdateStudentRequest } from '../models/api-models/update-student.model'
   providedIn: 'root',
 })
 export class StudentService {
-  private baseApiUrl = 'https://localhost:44338';
+  private baseApiUrl = environment.baseApiUrl;
   constructor(private httpClient: HttpClient) {}
 
   getStudents(): Observable<Student[]> {
